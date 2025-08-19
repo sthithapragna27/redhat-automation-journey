8cbd1213d9f657bdfd20defbd08509a53a238611
sthithapragna27 <lakkadiabhilashreddy@gmail.com>
2025-08-18 23:26:29 -0400

    Adding system_check.py automation script

---
+++ system_check.py
@@ -0,0 +1,8 @@
+import platform
+import os
+
+print("=== My Computer Info ===")
+print("Operating System:", platform.system())
+print("Computer Name:", platform.node())
+print("Current User:", os.getenv(USERNAME))
+print("Python Version:", Platform.python_version())
