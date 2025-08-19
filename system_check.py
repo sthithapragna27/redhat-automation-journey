4e642c30d3ef0ca18c19896b281a9e203b08fb4d
sthithapragna <lakkadiabhilashreddy@gmail.com>
2025-08-18 23:29:43 -0400

    Update system_check.py

--- system_check.py
+++ system_check.py
@@ -4,5 +4,5 @@ import os
 print("=== My Computer Info ===")
 print("Operating System:", platform.system())
 print("Computer Name:", platform.node())
-print("Current User:", os.getenv(USERNAME))
+print("Current User:", os.getenv("USERNAME"))
 print("Python Version:", Platform.python_version())
